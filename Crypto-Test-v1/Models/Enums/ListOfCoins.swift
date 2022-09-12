95f0bb7fb0f008a2787c5f34ca2fdca34dea5b83
Alexander <abrossimov@yandex.ru>
2022-09-12 17:33:49 +0300

    all coins in enum

--- Crypto-Test-v1/Models/Enums/ListOfCoins.swift
+++ Crypto-Test-v1/Models/Enums/ListOfCoins.swift
@@ -19,11 +19,11 @@ import Foundation
 enum ListOfCoins: String, CaseIterable {
    case btc = "btc"
    case eth = "eth"
-//   case tron = "tron"
-//   case polkadot = "polkadot"
-//   case dogecoin = "dogecoin"
-//   case tether = "tether"
-//   case stellar = "stellar"
-//   case cardano = "cardano"
-//   case xrp = "xrp"
+   case tron = "tron"
+   case polkadot = "polkadot"
+   case dogecoin = "dogecoin"
+   case tether = "tether"
+   case stellar = "stellar"
+   case cardano = "cardano"
+   case xrp = "xrp"
 }
